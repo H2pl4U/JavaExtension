8b661cc452652b1c0ce1f89faaf0e100cb4f84a8
liuwei <wei.liu@shmedo.cn>
2020-09-30 15:11:34 +0800

    strategy

--- src/main/java/com/h2pl4u/designpattern/Strategy.java
+++ src/main/java/com/h2pl4u/designpattern/Strategy.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 策略模式
+ * 策略模式定义了算法家族，分别封装起来，让它们之间可以互相替换。此模式让算法的变化不
+ * 会影响到使用算法的用户。策略模式常用于消除大量的if else代码。
+ * 适用场景：
+ * 系统有很多类，它们的区别仅仅在于行为不同；
+ * 一个系统需要动态地在几种算法中选择一种；
  * Created by Liuwei on 2020/9/30 14:32
  */
 public class Strategy {
