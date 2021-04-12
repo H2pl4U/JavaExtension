69af0d86b37e28268c004653e7640cd7e410ba5e
liuwei <wei.liu@shmedo.cn>
2021-04-12 14:29:16 +0800

    future task

---
+++ src/main/java/com/h2pl4u/juc/CompletableFutureDemo.java
@@ -0,0 +1,9 @@
+package com.h2pl4u.juc;
+
+/**
+ * Created on 2021/4/12 14:07
+ *
+ * @Author Liuwei
+ */
+public class CompletableFutureDemo {
+}
---
+++ src/main/java/com/h2pl4u/juc/FutureDemo.java
@@ -0,0 +1,9 @@
+package com.h2pl4u.juc;
+
+/**
+ * Created on 2021/4/12 13:29
+ *
+ * @Author Liuwei
+ */
+public class FutureDemo {
+}
